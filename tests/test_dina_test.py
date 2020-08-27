f39dda70f13418103b89a11a88862a03699a772b
dinabandhu50 <beheradinabandhu50@gmail.com>
2020-08-27 07:16:47 +0530

    first commit

---
+++ tests/test_dina_test.py
@@ -0,0 +1,10 @@
+from dina_test import __version__
+
+# from dinaTest import print_random
+# import numpy as np
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+    # assert isinstance(print_random(), type(np.array()))
